855baf1dbb916e469c8bb3a567fcc153f896df96
chenyyxx <chenyx.eric2013@gmail.com>
2019-11-20 20:55:58 -0600

    Initial updates for front end and back end

---
+++ server/src/main/java/com/ai/tweetsTrend/TweetsTrendApplication.java
@@ -0,0 +1,13 @@
+package com.ai.tweetsTrend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetsTrendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetsTrendApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/ai/tweetsTrend/TweetsTrendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.tweetsTrend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweetsTrendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
